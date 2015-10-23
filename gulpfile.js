d83b66a39bc6ccee8e513f2c940820126d94b079
Paul Burdick <paul@reedmaniac.com>
2015-10-22 17:14:14 -0700

    Switch reedmaniac.com to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,36 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+
+      // Fonts need to be put in the correct spots for CSS versioning
+	  mix.copy('./resources/assets/font-awesome/fonts/fontawesome-webfont.*', './public/build/fonts/');
+	  mix.copy('./resources/assets/font-awesome/fonts/FontAwesome.otf', './public/build/fonts/');
+	  mix.copy('./node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.*', './public/build/fonts/bootstrap/');
+
+	  mix.sass([
+	    	'app.scss',
+	       ],
+	       './public/css/sass.css')
+
+	      .styles([
+	         '../font-awesome/css/font-awesome.css',
+	         './public/css/sass.css'
+	      ],
+	      './public/css/all.css')
+	      .scripts([
+			'./node_modules/jquery/dist/jquery.js',
+			'./node_modules/bootstrap/dist/js/bootstrap.js',
+			'./resources/assets/js/jquery.easing.min.js',
+			'./resources/assets/js/classie.js',
+			'./resources/assets/js/cbpAnimatedHeader.js',
+			'./resources/assets/js/jqBootstrapValidation.js',
+			'./resources/assets/js/contact_me.js',
+			'./resources/assets/js/app.js'
+		],
+		'./public/js/app.js')
+		.version(['public/css/all.css', 'public/js/app.js']);
+
+	    // We combine our SASS CSS file into our all.css, but browsers complain without mapping file.
+	    mix.copy('./public/css/sass.css.map', './public/build/css/');
+
 });
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Paul Burdick :: Home</title>
+
+
+    <!-- Custom Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{ elixir('css/all.css') }}">
+
+    
+    <meta name="description" content="Coder. Geek. Vagabond. Thru-Hiker. Lover of Knowledge. Pursuer of Fun.">
+    <meta name="author" content="Paul Burdick">
+    
+    <meta property="og:site_name" content="Paul Burdick" />
+	<meta property="og:description" content="Coder. Geek. Vagabond. Thru-Hiker. Lover of Knowledge. Pursuer of Fun." />
+
+	<meta property="twitter:creator" content="reedmaniac" />
+
+</head>
+
+<body id="page-top" class="index">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">Paul Burdick</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#passions">Passions</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">About</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">More <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                             <li>
+                                <a href="https://www.flickr.com/photos/133323973@N07/">Photos</a>
+                            </li>
+                            <li>
+                                <a href="https://www.behance.net/reedmaniac">Portfolio</a>
+                            </li>
+                            <li>
+                                <a href="http://shapelesschaos.com">Blog</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="intro-text">
+            	<div class="row">
+					<div class="col-md-offset-5 col-sm-offset-4 col-xs-offset-4 col-md-2 col-sm-4 col-xs-4">
+						<div class="team-member">
+							<img src="img/paul-burdick-scruffy.jpeg" class="img-responsive img-circle" alt="Who's scruffy looking?">
+						</div>
+					</div>
+				</div>
+                <div class="intro-lead-in">Coder. Geek. Outdoorsman.</div>
+                <hr class="intro-divider" />
+                <ul class="list-inline intro-social-buttons">
+					<li>
+						<a href="https://twitter.com/reedmaniac" class="btn btn-paul" title="Twitter"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+					</li>
+					<li>
+						<a href="https://github.com/reedmaniac" class="btn btn-paul" title="GitHub"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+					</li>
+					<li>
+						<a href="https://www.linkedin.com/in/paulburdick" class="btn btn-paul" title="LinkedIn"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
+					</li>
+				</ul>
+            </div>
+        </div>
+    </header>
+    
+    <!-- Endeavors Section -->
+    <section id="passions" class="bg-light-gray">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Passions</h2>
+                    <h3 class="section-subheading text-muted">Less of a young professional - more of an ancient amateur.</h3>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fa fa-laptop fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="service-heading">Code</h4>
+                    <p class="text-muted">Chief Technology Officer during the building of 
+                    CodeIgniter and ExpressionEngine. Built a massive, scalable RESTful API for the
+                    ImpactFlow SaaS. Talented at understanding in-depth
+                    technical details while keeping an eye on the bigger picture.</p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fa fa-space-shuttle fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="service-heading">Geek</h4>
+                    <p class="text-muted">Eternal student of Nature, Science and Technology, Medicine, and Philosophy.
+                    Wrote thesis on the Right of Privacy and went back to school for Pre-Medicine. Programmer and designer.
+                    Wilderness First Responder and outdoor guide. Always more to learn.</p>
+                </div>
+                <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fa fa-tree fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <h4 class="service-heading">Outdoorsman</h4>
+                    <p class="text-muted">
+                    Runner of trails, climber of Cascades, long distance biker, and a dabbler in kayaking and rafting.
+                    Thru-hiked the Pacific Crest Trail in 2013. Have survived blizzards on Mt. Hood and scorching days in the Mojave. Never ask me about couscous.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- About Section -->
+    <section id="about" class="">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">About</h2>
+                    <h3 class="section-subheading text-muted">In case of emergency?  RUN!</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-offset-1 col-sm-offset-2 col-md-offset-2 col-lg-offset-2 col-xs-10 col-sm-8 about-content">
+                    
+                   <p>Paul Burdick is a 36 year old programmer, adventurer, and all around geek who is unwilling
+to call himself an enterpreneur because he refuses to put on airs. He frequently dabbles
+in startup life and has been the Chief Technology Officer for both <a href="http://ellislab.com">EllisLab</a> (creator of ExpressionEngine and CodeIgniter)
+and <a href="https://impactflow.com">ImpactFlow</a> . After his time at EllisLab, he joined up with the fine folks at 
+<a href="http://solspace.com">Solspace</a> and explored the realm of client work by working with the likes of Nokia, Turner Broadcasting System (TBS), IDEO, and Broadcast Music, Inc (BMI). </p>
+
+
+<p>He has a degree in Philosophy from <a href="http://www.reed.edu">Reed College</a> and returned to school a decade later to attend <a href="https://www.pdx.edu">Portland State University</a> for Pre-Medicine.
+Since 2003, he has been a certified <a href="http://www.nols.edu/wmi/courses/wfr.shtml">Wilderness First Responder</a> and later was a licensed Wilderness EMT for four years in Oregon. 
+He interned at the Portland State Outdoor Program and has used his wilderness medicine expertise and backcountry experience
+to guide trips around the Pacific Northwest for both schools and nonprofits.</p>
+
+<p>In 2013, after a long recovery from a knee injury, Paul completed a thru-hike of 
+the <a href="http://www.pcta.org">Pacific Crest Trail</a> and discovered how truly magical modern
+marvels like internal plumbing really are. He is an intermediate mountaineer who has 
+climbed multiple Cascade volcanoes as well as Mt. Whitney and Longs Peak. Other passions include
+trail running–the chillier and muddier the better–and long distance biking. Lately he has been considering a winter
+thru-hike of the Appalachian Trail.</p>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Contact Section -->
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Contact Me</h2>
+                    <h3 class="section-subheading text-muted">Always interested in a worthwhile coding project or an incredible adventure.</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <form name="sentMessage" id="contactForm" novalidate>
+                        <input type="hidden" name="token" value="<?php echo $token; ?>">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Your Name *" id="name" required data-validation-required-message="Please enter your name.">
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control" placeholder="Your Email *" id="email" required data-validation-required-message="Please enter your email address.">
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                                <div class="form-group">
+                                    <input type="tel" class="form-control" placeholder="Your Phone" id="phone">
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <textarea class="form-control" placeholder="Your Message *" id="message" required data-validation-required-message="Please enter a message."></textarea>
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="col-lg-12 text-center">
+                                <div id="success"></div>
+                                <button type="submit" class="btn btn-paul js-submit-message">Send Message</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <span class="copyright">Copyright &copy; Paul Burdick 2015</span>
+                </div>
+                <div class="col-md-4">
+                </div>
+                <div class="col-md-4">
+                    <ul class="list-inline social-buttons">
+                        <li><a href="https://twitter.com/reedmaniac" title="Twitter"><i class="fa fa-twitter"></i></a>
+                        </li>
+                        <li><a href="https://github.com/reedmaniac" title="GitHub"><i class="fa fa-github"></i></a>
+                        </li>
+                        <li><a href="https://www.linkedin.com/in/paulburdick" title="LinkedIn"><i class="fa fa-linkedin"></i></a>
+                        </li>
+                        <li><a href="https://www.flickr.com/photos/133323973@N07/" title="Flickr"><i class="fa fa-flickr"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+    <script src="{{ elixir('js/app.js') }}"></script>
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-69209507-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+
+</body>
+
+</html>
