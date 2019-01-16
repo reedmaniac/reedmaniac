01680ee1ba4269947d102f68c11872e4f1f88a12
Paul Burdick <paul@reedmaniac.com>
2019-01-16 11:00:12 -0800

    Trying to use DotEnv for configuration

--- craft/config/db.php
+++ craft/config/db.php
@@ -7,29 +7,12 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
 
-return array(
-    '*' => array(
-        'tablePrefix' => 'craft',
-    ),
-
-    'reedmaniac.dev' => array(
-        'server'    => 'localhost',
-        'user'      => 'homestead',
-        'password'  => 'secret',
-        'database'  => 'reedmaniac',
-    ),
-
-    'staging.reedmaniac.com' => array(
-        'server'    => 'localhost',
-        'user'      => 'forge',
-        'password'  => '3tUVemUka9ZY4RTlJ3bp',
-        'database'  => 'blog-dev',
-    ),
-
-     'reedmaniac.com' => array(
-        'server'    => 'localhost',
-        'user'      => 'forge',
-        'password'  => '3tUVemUka9ZY4RTlJ3bp',
-        'database'  => 'blog',
-    )
-);
+return [
+    '*' => [
+		'tablePrefix' => 'craft',
+		'server' => getenv('DB_HOST'),
+		'user' => getenv('DB_USERNAME'),
+		'password' => getenv('DB_PASSWORD'),
+		'database' => getenv('DB_DATABASE'),
+    ],
+];
