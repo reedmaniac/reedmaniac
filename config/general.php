27818781ab6607039542ff455226ff5b0c01b1e4
Paul Burdick <paul@reedmaniac.com>
2019-01-16 12:58:56 -0800

    Upgrading to Craft CMS 3

---
+++ config/general.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ *
+ * @see craft\config\GeneralConfig
+ */
+
+return [
+    // Global settings
+    '*' => [
+        // Default Week Start Day (0 = Sunday, 1 = Monday...)
+        'defaultWeekStartDay' => 0,
+
+        // Enable CSRF Protection (recommended)
+        'enableCsrfProtection' => true,
+
+        // Whether generated URLs should omit "index.php"
+        'omitScriptNameInUrls' => true,
+
+        // Control Panel trigger word
+        'cpTrigger' => getenv('CP_TRIGGER'),
+
+        // The secure key Craft will use for hashing and encrypting data
+        'securityKey' => getenv('SECURITY_KEY'),
+        
+        'siteUrl' => getenv('APP_URL'),
+        
+        // Dev Mode (see https://craftcms.com/support/dev-mode)
+        'devMode' => getenv('APP_DEBUG')
+    ],
+
+    // Dev environment settings
+    'dev' => [
+        
+    ],
+
+    // Staging environment settings
+    'staging' => [
+        
+    ],
+
+    // Production environment settings
+    'production' => [
+        
+    ],
+];
--- craft/Notes.md
+++
@@ -1,2 +0,0 @@
-
-For some reason my Pro License stopped working.  Search for "hasWrongEdition" and have check return null.  Go into craft_info table and set edition to 2.
